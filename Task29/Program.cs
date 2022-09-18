f4c90a3eb6aa31ccbbab2326e4a1c7d05c472a11
Marina Vystavkina <vistavkinamm@mail.ru>
2022-09-18 17:47:50 +0300

    Task_29

---
+++ Task29/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] numbers = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(0, 11);
+    Console.Write(" " + MethodArray (i) + " ");
+ }
+Console.Write("]");
+
+int MethodArray (int a)
+{
+    return numbers[a];
+}
