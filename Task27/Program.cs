8e2989de3fd3756671ddef9dfe950b8d4c3b30d8
Marina Vystavkina <vistavkinamm@mail.ru>
2022-09-18 18:04:35 +0300

    Task_27

---
+++ Task27/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int num=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сумма цифр числа: " +GetSum(num));
+
+int GetSum(int number)
+{
+    int sum=0;
+    while(number>0)
+    {
+        sum+=number%10;
+        number/=10;
+    }
+    return sum;
+}
